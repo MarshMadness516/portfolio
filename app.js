003630fa0d00e3d2a3d5dc21f652df018600dba5
Marshall Pritchett <mpritchett516@gmail.com>
2019-05-23 17:33:22 +0000

    create views directory and render landing page

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.send('home page');
+	res.render('landing');
 });
 
 app.listen(3000, () => {
